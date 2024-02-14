5fc20afa1fd195ba5bb1289cb567c800d1f20bef
Italo Valcy <idasilva@fiu.edu>
2024-02-14 20:42:18 +0000

    Making ORCID credentials configurable (Issue #23)

--- docker_for_build/docker-entrypoint.sh
+++ docker_for_build/docker-entrypoint.sh
@@ -5,8 +5,12 @@ cp $MEICAN_DIR/docker_for_build/db.php $MEICAN_DIR/config/ \
  && sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/" $MEICAN_DIR/config/db.php \
  && sed -i "s/MYSQL_USER/$MYSQL_USER/" $MEICAN_DIR/config/db.php \
  && sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" $MEICAN_DIR/config/db.php \
+ && sed -i "s#XXX_MEICAN_HOST_XXX#$MEICAN_HOST#g" $MEICAN_DIR/web/index.php \
+ && sed -i "s#XXX_SDX_CONTROLLER_URL_XXX#$SDX_CONTROLLER_URL#g" $MEICAN_DIR/web/index.php \
+ && sed -i "s#XXX_ORCID_CLIENT_ID_XXX#$ORCID_CLIENT_ID#g" $MEICAN_DIR/web/index.php \
+ && sed -i "s#XXX_ORCID_CLIENT_SECRET_XXX#$ORCID_CLIENT_SECRET#g" $MEICAN_DIR/web/index.php \
  && chown meican:meican $MEICAN_DIR/config/db.php  \
- && mkdir $MEICAN_DIR/vendor \
+ && mkdir -p $MEICAN_DIR/vendor \
  && chmod 777 $MEICAN_DIR/vendor $MEICAN_DIR/web/assets $MEICAN_DIR/runtime \
  && su meican -c "php composer.phar install" \
  && service apache2 start \
