be2eb7b74694b45e8d89db8dd06e6be048dad765
ampath <ampath@88e9fe78cca2.p.fiu.edu>
2024-03-27 11:22:07 -0400

    storing token in new table meican_cilogon_auth

--- modules/circuits/controllers/NodesController.php
+++ modules/circuits/controllers/NodesController.php
@@ -8,6 +8,7 @@ namespace meican\circuits\controllers;
 
 use Yii;
 use yii\helpers\Url;
+use DateTime;
 use yii\data\ActiveDataProvider;
 use yii\helpers\Json;
 
@@ -29,6 +30,8 @@ use meican\topology\models\Port;
 use meican\topology\models\Domain;
 use meican\topology\models\Network;
 use meican\topology\models\Service;
+
+// include(__DIR__ . '/../../../config/db.php');
 
 /**
  * @author Maurício Quatrin Guerreiros
@@ -70,14 +73,138 @@ class NodesController extends RbacController {
     }
     
     public function actionShow() {   // this function manages the mapping of SDX-topology and displays on the MEICAN UI
-   
+      
+      $api_url=API_URL;
+      $meican_url=MEICAN_URL;
+
+      $userId = Yii::$app->user->id;
+      $rows = (new \yii\db\Query())
+          ->select(['token', 'expiration'])
+          ->from('meican_cilogon_auth')
+          ->where(['user_id' => $userId])
+          ->all();
+      
+      date_default_timezone_set('America/New_York');
+      $token_date = isset($rows[0]['expiration']) ? $rows[0]['expiration'] : null;
+      $token = isset($rows[0]['token']) ? $rows[0]['token'] : null;
+      if ($token_date !== null && $token !== null) {
+
+        $expiration_datetime = DateTime::createFromFormat('Y-m-d H:i:s', $token_date);
+        $curr_datetime = new DateTime();
+
+        if ($expiration_datetime !== null && $curr_datetime !== null) {
+            $diff = $curr_datetime->diff($expiration_datetime);
+            $total_seconds = $expiration_datetime->getTimestamp() - $curr_datetime->getTimestamp();
+
+        } else {
+            header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+            exit();
+        }
+
+        if (!empty($rows[0]['token']) && isset($total_seconds) && in_array($total_seconds, range(1,172800))){
+          header("Location: https://".$meican_url."/circuits/nodes/show");
+        }
+        else if (!empty($rows[0]['token']) && isset($total_seconds) && ($total_seconds > 172800 || $total_seconds <= 0 )) {
+          $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+          if (strpos($actual_link,'code') !== false) {
+              $code=$_GET['code'];
+  
+            $curl = curl_init();
+  
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://cilogon.org/oauth2/token?grant_type=authorization_code&client_id=cilogon%3A%2Fclient_id%2F25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https%3A%2F%2F'.$meican_url.'%2Fcircuits%2Fnodes%2Fshow&client_secret=Vib4sbOGQXI8tt9s-uQ16hQnJlNUxUsAvAMISiCJBPFlLf2c82ZyUDKBYpv-eNX6qV-wW-OzRzoAaVslxCsozQ&code='.$code.'',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+  
+            $response = curl_exec($curl);
+  
+            curl_close($curl);
+            $response_arr=json_decode($response,true);
+            if(array_key_exists('access_token',$response_arr)){
+              $access_token=$response_arr['access_token'];
+              
+              $expiration_datetime = new DateTime();
+              $expiration_datetime->modify('+2 days');
+              $result = Yii::$app->db->createCommand()->update(
+                'meican_cilogon_auth',
+                ['token' => $access_token, 'expiration' => $expiration_datetime->format('Y-m-d H:i:s')],
+                'user_id = ' .$userId
+              )->execute();
+            }
+            else{
+              header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+              exit();
+            } 
+          }
+          else {
+            header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+            exit();
+          }
+        }
+        else {
+          header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+            exit();
+        }
+        
+      }
+      else {
+        $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        if (strpos($actual_link,'code') !== false) {
+            $code=$_GET['code'];
+
+          $curl = curl_init();
+
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://cilogon.org/oauth2/token?grant_type=authorization_code&client_id=cilogon%3A%2Fclient_id%2F25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https%3A%2F%2F'.$meican_url.'%2Fcircuits%2Fnodes%2Fshow&client_secret=Vib4sbOGQXI8tt9s-uQ16hQnJlNUxUsAvAMISiCJBPFlLf2c82ZyUDKBYpv-eNX6qV-wW-OzRzoAaVslxCsozQ&code='.$code.'',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+          ));
+
+          $response = curl_exec($curl);
+
+          curl_close($curl);
+          $response_arr=json_decode($response,true);
+          if(array_key_exists('access_token',$response_arr)){
+            $access_token=$response_arr['access_token'];
+
+            $expiration_datetime = new DateTime();
+            $expiration_datetime->modify('+2 days');
+
+            $result = Yii::$app->db->createCommand()->insert('meican_cilogon_auth', [
+              'user_id' => $userId,
+              'token' => $access_token,
+              'expiration' => $expiration_datetime->format('Y-m-d H:i:s')
+              ])->execute();
+          }
+          else{
+            header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+            exit();
+          } 
+        }
+        else {
+          header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/25669b5df0f0c45880aa56c410bd32b9&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
+          exit();
+        }
+      }
+
     if(!self::can("sdxCircuit/create")){
             return $this->goHome();
         }
 
     //calling API for topology
-    $api_url=API_URL;
-    $meican_url=MEICAN_URL;
     $curl = curl_init();
 
     /* CURL request to SDX-Controller endpoint for getting topology and mapping on the MEICAN UI */
