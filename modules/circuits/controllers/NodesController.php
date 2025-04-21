29dd949194937a7f8e101e50c7679386f4222670
usmanazFIU <muhaziz@fiu.edu>
2025-04-21 10:35:43 -0500

    ownership attribute

--- modules/circuits/controllers/NodesController.php
+++ modules/circuits/controllers/NodesController.php
@@ -346,35 +346,7 @@ class NodesController extends RbacController {
         $userId = Yii::$app->user->id;
         
         if($userId != 1) { // Removing CILogon for Admin User
-          // Checks for the user entry in the meican_cilogon_auth table
-          // $rows = (new \yii\db\Query())
-          // ->select(['token', 'expiration'])
-          // ->from('meican_cilogon_auth')
-          // ->where(['user_id' => $userId])
-          // ->all();
-
-          // date_default_timezone_set('America/New_York');
-          // $token_date = isset($rows[0]['expiration']) ? $rows[0]['expiration'] : null;
-          // $token = isset($rows[0]['token']) ? $rows[0]['token'] : null;
-          // if ($token_date !== null && $token !== null) { // If Record Present
-
-            //$expiration_datetime = DateTime::createFromFormat('Y-m-d H:i:s', $token_date);
-            //$curr_datetime = new DateTime();
-
-            // token expiration check (2 days)
-            // if ($expiration_datetime !== null && $curr_datetime !== null) {
-            //     $diff = $curr_datetime->diff($expiration_datetime);
-            //     $total_seconds = $expiration_datetime->getTimestamp() - $curr_datetime->getTimestamp();
-
-            // } else {
-            //     header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
-            //     exit();
-            // }
-
-            // if (!empty($rows[0]['token']) && isset($total_seconds) && in_array($total_seconds, range(1,172800))){ // token is not expired
-            //   header("Location: https://".$meican_url."/circuits/nodes/show");
-            // }
-            // else if (!empty($rows[0]['token']) && isset($total_seconds) && ($total_seconds > 172800 || $total_seconds <= 0 )) { // token expired
+              
               $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
               if (strpos($actual_link,'code') !== false) {
@@ -399,8 +371,6 @@ class NodesController extends RbacController {
                 $response_arr=json_decode($response,true);
                 if(array_key_exists('access_token',$response_arr)){
                   $access_token=$response_arr['access_token'];
-                  //echo $access_token;
-                  //echo $response_arr['id_token'];
 
                   $curl = curl_init();
       
@@ -427,86 +397,12 @@ class NodesController extends RbacController {
                 $trimmedOutput = substr($base64Encoded, 0, 16); // Get first 16 characters
 
 
-                
-                //echo $sub;
-                //echo $subExtract;
-                //echo $hashedString;
-                //echo $trimmedOutput;
-                  
-                  // // updating the token and expiration date in meican_cilogon_auth table for the respective user
-                  // $expiration_datetime = new DateTime();
-                  // $expiration_datetime->modify('+2 days');
-                  // $result = Yii::$app->db->createCommand()->update(
-                  //   'meican_cilogon_auth',
-                  //   ['token' => $access_token, 'expiration' => $expiration_datetime->format('Y-m-d H:i:s')],
-                  //   'user_id = ' .$userId
-                  // )->execute();
                 }
               }
                 else{
                   header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
                   exit();
                 } 
-          //     }
-          //     else {
-          //       header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
-          //       exit();
-          //     }
-          //   }
-          //   else {
-          //     header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
-          //       exit();
-          //   }
-            
-          // }
-      //     else { // Generate the new token and insert in meican_cilogon_auth table
-      //       $actual_link = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
-      //       if (strpos($actual_link,'code') !== false) {
-      //           $code=$_GET['code'];
-
-      //         $curl = curl_init();
-
-      //         curl_setopt_array($curl, array(
-      //           CURLOPT_URL => 'https://cilogon.org/oauth2/token?grant_type=authorization_code&client_id=cilogon%3A%2Fclient_id%2F'.$CILogonClientID.'&redirect_uri=https%3A%2F%2F'.$meican_url.'%2Fcircuits%2Fnodes%2Fshow&client_secret='.$CILogonClientSecret.'&code='.$code.'',
-      //           CURLOPT_RETURNTRANSFER => true,
-      //           CURLOPT_ENCODING => '',
-      //           CURLOPT_MAXREDIRS => 10,
-      //           CURLOPT_TIMEOUT => 0,
-      //           CURLOPT_FOLLOWLOCATION => true,
-      //           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      //           CURLOPT_CUSTOMREQUEST => 'GET',
-      //         ));
-
-      //         $response = curl_exec($curl);
-
-      //         curl_close($curl);
-      //         $response_arr=json_decode($response,true);
-      //         if(array_key_exists('access_token',$response_arr)){
-      //           $access_token=$response_arr['access_token'];
-
-      //           // setting expiration date for 2 days 
-      //           $expiration_datetime = new DateTime();
-      //           $expiration_datetime->modify('+2 days');
-
-      //           // Inserting the token expiration date in meican_cilogon_auth table
-      //           $result = Yii::$app->db->createCommand()->insert('meican_cilogon_auth', [
-      //             'user_id' => $userId,
-      //             'token' => $access_token,
-      //             'expiration' => $expiration_datetime->format('Y-m-d H:i:s')
-      //             ])->execute();
-      //         }
-      //         else{
-      //           header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
-      //           exit();
-      //         } 
-      //       }
-      //       else {
-      //         header("Location: https://cilogon.org/authorize?response_type=code&client_id=cilogon:/client_id/".$CILogonClientID."&redirect_uri=https://".$meican_url."/circuits/nodes/show&scope=openid+profile+email");
-      //         exit();
-      //       }
-      //     }
-      //   }
        }
      }
 
